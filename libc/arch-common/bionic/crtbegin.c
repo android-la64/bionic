6fafb7e0a220cd764e1b0b31425454a1d8417234
caiyinyu <caiyinyu@loongson.cn>
2023-10-10 16:58:53 +0800

    tmp-import-riscv/0149-Add-riscv64-crtbegin-assembler.patch
    Change-Id: Ia985b6365777711c64a78733455704ef642c1ae2

--- libc/arch-common/bionic/crtbegin.c
+++ libc/arch-common/bionic/crtbegin.c
@@ -52,6 +52,8 @@ __used static void _start_main(void* raw_args) {
 __asm__(PRE "bti j; mov x29,#0; mov x30,#0; mov x0,sp; b _start_main" POST);
 #elif defined(__arm__)
 __asm__(PRE "mov fp,#0; mov lr,#0; mov r0,sp; b _start_main" POST);
+#elif defined(__loongarch__)
+__asm__(PRE "li.d $fp,0; li.d $ra,0; move $a0,$sp; b _start_main" POST);
 #elif defined(__i386__)
 __asm__(PRE
         "xorl %ebp,%ebp; movl %esp,%eax; andl $~0xf,%esp; subl $12,%esp; pushl %eax;"
