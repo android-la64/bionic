478af6786e9f50f0082bdb8a34b89fa406d4f3d5
caiyinyu <caiyinyu@loongson.cn>
2023-10-26 10:18:44 +0800

    add user_regs_struct/user_fp_struct

--- libc/include/sys/user.h
+++ libc/include/sys/user.h
@@ -234,8 +234,19 @@ struct user_fpsimd_struct {
 
 #elif defined(__loongarch__)
 
-// This space deliberately left blank for now.
-// No other libcs have any loongarch64-specific structs.
+struct user_regs_struct
+{
+  uint64_t gpr[32];
+  uint64_t pc;
+  uint64_t badvaddr;
+  uint64_t reserved[11];
+};
+
+struct user_fp_struct {
+  uint64_t    fpr[32];
+  uint64_t    fcc;
+  uint32_t    fcsr;
+};
 
 #else
 
