aee2142cc656c92d25eb89702e432709527e2b2a
caiyinyu <caiyinyu@loongson.cn>
2023-12-14 15:50:13 +0800

    Update auto generated header files.
    update baseline kernel_common origin/a12_larch:
    
    commit 9967ce8f535eeea70bd510774d6eb4bdd52289ae (HEAD -> a12_larch, origin/a12_larch)
    Author: Huacai Chen <chenhuacai@loongson.cn>
    Date:   Mon Oct 23 17:13:34 2023 +0800
    
        LoongArch: Update Loongson-3 default config file for Android
    
        Change-Id: Icf1d69d6f62a63afdbebf351306c470660abb6c8
        Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- libc/kernel/android/scsi/scsi/scsi_ioctl.h
+++ libc/kernel/android/scsi/scsi/scsi_ioctl.h
@@ -1,50 +1,31 @@
-/* SPDX-License-Identifier: GPL-2.0 */
+/****************************************************************************
+ ****************************************************************************
+ ***
+ ***   This header was automatically generated from a Linux kernel header
+ ***   of the same name, to make information necessary for userspace to
+ ***   call into the kernel available to libc.  It contains only constants,
+ ***   structures, and macros generated from the original header, and thus,
+ ***   contains no copyrightable information.
+ ***
+ ***   To edit the content of this header, modify the corresponding
+ ***   source file (e.g. under external/kernel-headers/original/) then
+ ***   run bionic/libc/kernel/tools/update_all.py
+ ***
+ ***   Any manual change here will be lost the next time this script will
+ ***   be run. You've been warned!
+ ***
+ ****************************************************************************
+ ****************************************************************************/
 #ifndef _SCSI_IOCTL_H
-#define _SCSI_IOCTL_H 
-
+#define _SCSI_IOCTL_H
 #define SCSI_IOCTL_SEND_COMMAND 1
 #define SCSI_IOCTL_TEST_UNIT_READY 2
 #define SCSI_IOCTL_BENCHMARK_COMMAND 3
-#define SCSI_IOCTL_SYNC 4			/* Request synchronous parameters */
+#define SCSI_IOCTL_SYNC 4
 #define SCSI_IOCTL_START_UNIT 5
 #define SCSI_IOCTL_STOP_UNIT 6
-/* The door lock/unlock constants are compatible with Sun constants for
-   the cdrom */
-#define SCSI_IOCTL_DOORLOCK 0x5380		/* lock the eject mechanism */
-#define SCSI_IOCTL_DOORUNLOCK 0x5381		/* unlock the mechanism	  */
-
-#define	SCSI_REMOVAL_PREVENT	1
-#define	SCSI_REMOVAL_ALLOW	0
-
-#ifdef __KERNEL__
-
-struct scsi_device;
-
-/*
- * Structures used for scsi_ioctl et al.
- */
-
-typedef struct scsi_ioctl_command {
-	unsigned int inlen;
-	unsigned int outlen;
-	unsigned char data[];
-} Scsi_Ioctl_Command;
-
-typedef struct scsi_idlun {
-	__u32 dev_id;
-	__u32 host_unique_id;
-} Scsi_Idlun;
-
-/* Fibre Channel WWN, port_id struct */
-typedef struct scsi_fctargaddress {
-	__u32 host_port_id;
-	unsigned char host_wwn[8]; // include NULL term.
-} Scsi_FCTargAddress;
-
-int scsi_ioctl_block_when_processing_errors(struct scsi_device *sdev,
-		int cmd, bool ndelay);
-extern int scsi_ioctl(struct scsi_device *, int, void __user *);
-extern int scsi_compat_ioctl(struct scsi_device *sdev, int cmd, void __user *arg);
-
-#endif /* __KERNEL__ */
-#endif /* _SCSI_IOCTL_H */
+#define SCSI_IOCTL_DOORLOCK 0x5380
+#define SCSI_IOCTL_DOORUNLOCK 0x5381
+#define SCSI_REMOVAL_PREVENT 1
+#define SCSI_REMOVAL_ALLOW 0
+#endif
