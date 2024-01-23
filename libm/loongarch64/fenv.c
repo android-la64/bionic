08ed5138a6d536c36f4d04c69304b52303911778
caiyinyu <caiyinyu@loongson.cn>
2024-01-23 19:36:29 +0800

    Fix the following fails:
    fenv.feraiseexcept
    fenv.fegetenv_fesetenv
    fenv.feholdexcept_feupdateenv
    fenv.fegetexceptflag_fesetexceptflag

--- libm/loongarch64/fenv.c
+++ libm/loongarch64/fenv.c
@@ -112,56 +112,9 @@ int fegetexceptflag (fexcept_t *flagp, int excepts)
 
 int feraiseexcept (int excepts)
 {
-  const float fp_zero = 0.0f;
-  const float fp_one = 1.0f;
-  const float fp_max = FLT_MAX;
-  const float fp_min = FLT_MIN;
-  const float fp_1e32 = 1.0e32f;
-  const float fp_two = 2.0f;
-  const float fp_three = 3.0f;
+  fexcept_t ex = excepts;
 
-  /* Raise exceptions represented by EXPECTS.  But we must raise only
-     one signal at a time.  It is important that if the overflow/underflow
-     exception and the inexact exception are given at the same time,
-     the overflow/underflow exception follows the inexact exception.  */
-
-  /* First: invalid exception.  */
-  if (FE_INVALID & excepts)
-    __asm__ __volatile__("fdiv.s $f0,%0,%0\n\t"
-			 :
-			 : "f"(fp_zero)
-			 : "$f0");
-
-  /* Next: division by zero.  */
-  if (FE_DIVBYZERO & excepts)
-    __asm__ __volatile__("fdiv.s $f0,%0,%1\n\t"
-			 :
-			 : "f"(fp_one), "f"(fp_zero)
-			 : "$f0");
-
-  /* Next: overflow.  */
-  if (FE_OVERFLOW & excepts)
-    /* There's no way to raise overflow without also raising inexact.  */
-    __asm__ __volatile__("fadd.s $f0,%0,%1\n\t"
-			 :
-			 : "f"(fp_max), "f"(fp_1e32)
-			 : "$f0");
-
-  /* Next: underflow.  */
-  if (FE_UNDERFLOW & excepts)
-    __asm__ __volatile__("fdiv.s $f0,%0,%1\n\t"
-			 :
-			 : "f"(fp_min), "f"(fp_three)
-			 : "$f0");
-
-  /* Last: inexact.  */
-  if (FE_INEXACT & excepts)
-    __asm__ __volatile__("fdiv.s $f0, %0, %1\n\t"
-			 :
-			 : "f"(fp_two), "f"(fp_three)
-			 : "$f0");
-
-  /* Success.  */
+  fesetexceptflag(&ex, excepts);
   return 0;
 }
 
